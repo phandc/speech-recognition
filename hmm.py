47d045ee07101f63d0ebcaec2cb197671ce7ffa1
nhatcao <caominhnhat99@gmail.com>
2021-06-05 21:54:32 +0700

    Add some readme

--- hmm.py
+++ hmm.py
@@ -4,34 +4,20 @@ import os
 import math
 from sklearn.cluster import KMeans
 import hmmlearn.hmm
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
 import pickle
-
-
-def split_word(file_path):
-    audio_chunks = split_on_silence(file_path,
-                                    # must be silent for at least half a second
-                                    min_silence_len=500,
-
-                                    # consider it silent if quieter than -16 dBFS
-                                    silence_thresh=-16
-                                    )
-    for i, chunk in enumerate(audio_chunks):
-        out_file = ".//splitAudio//chunk{0}.wav".format(i)
-        print
-        "exporting", out_file
-        chunk.export(out_file, format="wav")
 
 
 def get_mfcc(file_path):
     y, sr = librosa.load(file_path) # read .wav file
     hop_length = math.floor(sr*0.010) # 10ms hop
     win_length = math.floor(sr*0.025) # 25ms frame
-    # mfcc is 12 x T matrix
+
+    # mfcc is 12 x T matrix with 12 MFCCs returns
+
     mfcc = librosa.feature.mfcc(
         y, sr, n_mfcc=12, n_fft=1024,
         hop_length=hop_length, win_length=win_length)
+
     # substract mean from mfcc --> normalize mfcc
     mfcc = mfcc - np.mean(mfcc, axis=1).reshape((-1,1))
     # delta feature 1st order and 2nd order
@@ -47,21 +33,14 @@ def get_class_data(data_dir):
     mfcc = [get_mfcc(os.path.join(data_dir,f)) for f in files if f.endswith(".wav")]
     return mfcc
 
-def clustering(X, n_clusters=38):
+def clustering(X, n_clusters=28):
     kmeans = KMeans(n_clusters=n_clusters, n_init=50, random_state=0, verbose=0)
     kmeans.fit(X)
     print("centers", kmeans.cluster_centers_.shape)
     return kmeans
 
 if __name__ == "__main__":
-
-
-    # "thông", "tin", "dịch", "bệnh", "covid", "mười", "test_thông", "test_tin", "test_dịch", "test_bệnh", "test_covid"
-
-    # class_names = ["thông", "tin", "dịch", "bệnh", "covid", "hôm", "nay","test_thông", "test_tin", "test_dịch", "test_bệnh", "test_covid","test_hôm", "test_nay"]
-
-    class_names = ["đội","việt","nam","tuyển","test_đội","test_tuyển","test_việt","test_nam"]
-
+    class_names = ["thông","tin","dịch","bệnh","covid","test_thông","test_tin", "test_dịch","test_bệnh","test_covid",]
     dataset = {}
     for cname in class_names:
         print(f"Load {cname} dataset")
@@ -75,7 +54,7 @@ if __name__ == "__main__":
     print("centers", kmeans.cluster_centers_.shape)
 
     #save kmeans models
-    with open("kmeans_s3.pkl", "wb") as f:
+    with open("kmeans.pkl", "wb") as f:
         pickle.dump(kmeans, f)
 
     models = {}
@@ -129,7 +108,7 @@ if __name__ == "__main__":
 
 
         # # save model
-        with open("model_s3.pkl", "wb") as f:
+        with open("model.pkl", "wb") as f:
             pickle.dump(models, f)
 
 
--- readme.md
+++ readme.md
@@ -1,7 +1,26 @@
 ## Speech Recognition Web Application
 ```
-To lauch app, please command:
+
+Introduction: 
+-The following project attempt to build a web based application to transfer speech to to text.
+-It can translate some Vietnamese words to short phases.
+-The model used for recognising is Hidden Markov Model
+-Words recognised by model are some Vietnamese words from joint dataset.
+
+
+Web app's basic user case:
+-Voice recording 
+-Delete record
+-Convert record to text
+-Download text 
+
+
+
+To lauch application, please command
    python app.py
-For speech recognition:
+
+For GMMHMM training
    python hmm.py
+Note: Code is a bit rusty. For custom training, make sure changing data path in hmm.py file
+
 ```
